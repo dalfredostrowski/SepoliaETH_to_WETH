20708ab043645a8acba672c6bdd8d1596f6e5f22
D. Alfred Ostrowski <115842612+dalfredostrowski@users.noreply.github.com>
2024-06-10 23:13:13 -0400

    Update hardhat.config.js

--- sepoliaDemo/hardhat.config.js
+++ sepoliaDemo/hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
         networks: {
                 sepolia: {
-                        url: "https://sepolia.infura.io/v3/16791f3a2d0f4a568461b0c3f287107a"
+                        url: "https://sepolia.infura.io/v3/167xxxxxxxxxxxxxxxxxxxxxxxxxxxxa"
             ,           chainID:  11155111
             ,           accounts: ['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx']
                 }
